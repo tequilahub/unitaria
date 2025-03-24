f99c6c88efa2dbbba83e8201f24d227dad202223
Matthias Deiml <matthias.deiml@uni-a.de>
2025-03-24 15:39:26 +0100

    Meeting 20.3.

--- bequem.py
+++ bequem.py
@@ -20,6 +20,9 @@ class Node:
     def simulate(self, input: np.array | None) -> np.array:
         raise NotImplementedError
 
+    def circuit(self) -> Circuit:
+        raise NotImplementedError
+
 
 class Projection:
     num_bits: int
@@ -32,6 +35,49 @@ class Mul(Node):
         for child in children:
             input = child.simulate(input)
         return input
+
+    def circuit(self) -> Circuit:
+        circuit = Circuit()
+        for child in children:
+            circuit.append(child.circuit())
+
+        return circuit
+
+
+class Solve(Node):
+
+    def __init__(self, A, b, condition):
+        super().__init__(self, children=[A, b], condition)
+
+    def __init__(self, children, data): # Muss existieren für alle Nodes
+        super().__init__(self, children)
+        assert len(children) == 2
+        assert data is None
+        assert not children[0].is_vector()
+        assert children[1].is_vector()
+
+    def simulate(self, input: np.array | None) -> np.array:
+        ...
+
+    def circuit(self) -> Circuit:
+        angles = solver_angles(self.condition)
+        A_circuit = A.circuit()
+        b_circuit = b.circuit()
+
+        a = AncillaBits()
+        d = DataBits()
+        circuit = Circuit(a, d)
+        circuit.append(A_circuit, d, a)
+        circuit.append(b_circuit, d, a)
+        circuit.rot(angle0, a)
+        circuit.append(A_circuit.inverse(), d, a)
+        circuit.rot(angle1, a)
+        circuit.append(A_circuit, d, a)
+        circuit.append(b_circuit, d, a)
+        circuit.append(b_circuit.inverse(), d, a)
+        circuit.rot(angle2, a)
+        circuit.append(A_circuit.inverse(), d, a)
+        circuit.rot(angle3, a)
 
 
 class Vec(Node):
@@ -72,6 +118,4 @@ def parse():
         registry["vec"](children=[], data="4585493898")
     catch BequemParseError:
         pass
-
-def execute_matrix(node: Node) -> np.array:
 
