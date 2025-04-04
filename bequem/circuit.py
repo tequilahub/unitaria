cf94d30be4444d2e975ae708bcc3513a37b566b8
ohuettenhofer <oliver.huettenhofer@gmail.com>
2025-04-04 12:00:29 +0200

    Update circuit.py and constant.py

--- bequem/circuit.py
+++ bequem/circuit.py
@@ -2,10 +2,25 @@ import tequila as tq
 import numpy as np
 from dataclasses import dataclass
 
+from tequila import BitNumbering
+
 
 @dataclass
 class Circuit:
-    circuit: tq.QCircuit
+    def __init__(self, tq_circuit: tq.QCircuit | None = None):
+        if tq_circuit is not None:
+            self.tq_circuit = tq_circuit
+        else:
+            self.tq_circuit = tq.QCircuit()
 
-    def simulate(self, input: str | None=None) -> np.array:
-        raise NotImplementedError
+    def simulate(self, input: np.array | None=None, **kwargs) -> np.array:
+        input = tq.QubitWaveFunction.from_array(input, BitNumbering.LSB)
+        result = tq.simulate(self.tq_circuit, initial_state=input, **kwargs)
+        return result.to_array(BitNumbering.LSB, copy=False)
+
+    def __add__(self, other):
+        return self.tq_circuit + other.tq_circuit
+
+    def __iadd__(self, other):
+        self.tq_circuit += other.tq_circuit
+        return self
---
+++ bequem/nodes/constant.py
@@ -0,0 +1,30 @@
+import numpy as np
+from tequila_building_blocks.state_prep import prepare_state
+
+from bequem.circuit import Circuit
+from bequem.node import Node
+from bequem.qubit_map import QubitMap, ZeroBit, IdBit
+
+
+class ConstantMatrix(Node):
+    def __init__(self, vec: np.array):
+        self.n_qubits = round(np.log2(vec.shape[0]))
+        assert 2 ** self.n_qubits == vec.shape[0]
+        self.vec = vec
+
+    def qubits_in(self) -> QubitMap:
+        return QubitMap([ZeroBit() for _ in range(self.n_qubits)])
+
+    def qubits_out(self) -> QubitMap:
+        return QubitMap([IdBit() for _ in range(self.n_qubits)])
+
+    def normalization(self) -> float:
+        return np.linalg.norm(self.vec)
+
+    def compute(self, input: np.array | None = None) -> np.array:
+        assert input is None
+        return self.vec / self.normalization()
+
+    def circuit(self) -> Circuit:
+        tq_circuit = prepare_state(self.vec, range(self.n_qubits))
+        return Circuit(tq_circuit)
