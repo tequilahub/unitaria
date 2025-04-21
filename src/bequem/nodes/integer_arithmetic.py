60f07304d261d70758151452b28d7cfadef6c3e3
ohuettenhofer <oliver.huettenhofer@gmail.com>
2025-04-21 20:06:06 +0200

    Implement integer addition

--- src/bequem/nodes/integer_arithmetic.py
+++ src/bequem/nodes/integer_arithmetic.py
@@ -1,5 +1,7 @@
 import numpy as np
 import tequila as tq
+from tequila_building_blocks.arithmetic import increment_circuit_single_ancilla, addition_circuit
+
 from .node import Node
 from bequem.qubit_map import QubitMap, ID
 from bequem.circuit import Circuit
@@ -12,10 +14,10 @@ class Increment(Node):
         self.bits = bits
 
     def qubits_in(self) -> QubitMap:
-        return QubitMap([ID for _ in range(self.bits)])
+        return QubitMap(self.bits, 1)
 
     def qubits_out(self) -> QubitMap:
-        return QubitMap([ID for _ in range(self.bits)])
+        return QubitMap(self.bits, 1)
 
     def normalization(self) -> float:
         return 1
@@ -24,7 +26,39 @@ class Increment(Node):
         return np.roll(input, 1, axis=-1)
 
     def circuit(self) -> Circuit:
-        circuit = tq.QCircuit()
-        for i in reversed(range(self.bits)):
-            circuit += tq.gates.X(target=i, control=list(range(i)))
+        circuit = increment_circuit_single_ancilla(target=list(reversed(range(self.bits))), ancilla=self.bits)
+        return Circuit(circuit)
+
+
+class IntegerAddition(Node):
+    def __init__(self, source_bits: int, target_bits: int):
+        # TODO: Restriction is because the ancilla free construction needs two source bits.
+        #  I know how to fix this but haven't implemented it yet.
+        if source_bits < 2 or target_bits < source_bits:
+            raise ValueError()
+        self.source_bits = source_bits
+        self.target_bits = target_bits
+
+    def qubits_in(self) -> QubitMap:
+        return QubitMap(self.source_bits + self.target_bits, 1)
+
+    def qubits_out(self) -> QubitMap:
+        return QubitMap(self.source_bits + self.target_bits, 1)
+
+    def normalization(self) -> float:
+        return 1
+
+    def compute(self, input: np.ndarray) -> np.ndarray:
+        N = 2 ** self.source_bits
+        M = 2 ** self.target_bits
+        input = input.reshape((-1, M, N))
+        result = np.zeros_like(input)
+        for val in range(N):
+            result[:, :, val] += np.roll(input[:, :, val], val, axis=-1)
+        return result.reshape(-1, M * N).squeeze()
+
+    def circuit(self) -> Circuit:
+        source = list(reversed(range(self.source_bits)))
+        target = list(reversed(range(self.source_bits, self.source_bits + self.target_bits)))
+        circuit = addition_circuit(source, target)
         return Circuit(circuit)
--- tests/nodes/test_integer_arithmetic.py
+++ tests/nodes/test_integer_arithmetic.py
@@ -1,6 +1,6 @@
 import pytest
 
-from bequem.nodes.integer_arithmetic import Increment
+from bequem.nodes.integer_arithmetic import Increment, IntegerAddition
 
 
 @pytest.mark.parametrize("bits", [-2, 0])
@@ -12,4 +12,13 @@ def test_increment_constructor(bits):
 @pytest.mark.parametrize("bits", [1, 2, 4])
 def test_increment(bits):
     A = Increment(bits)
+    A.verify()
+
+
+@pytest.mark.parametrize(
+    "source_bits, target_bits",
+    [(2, 2), (2, 3), (3, 3), (3, 4)]
+)
+def test_integer_addition(source_bits, target_bits):
+    A = IntegerAddition(source_bits, target_bits)
     A.verify()
