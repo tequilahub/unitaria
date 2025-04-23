bbed77604555e4791b1849dda08f4ab84b7d26b4
Matthias Deiml <matthias.deiml@uni-a.de>
2025-04-23 14:15:08 +0200

    Add comment and (failing) test for global phase in state preparation

--- tests/nodes/test_constant.py
+++ tests/nodes/test_constant.py
@@ -1,7 +1,16 @@
+import pytest
+
 import numpy as np
 from bequem.nodes.constant import ConstantVector
 
 
 def test_constant_matrix():
     A = ConstantVector(np.array([1, 2j, 1 / 3, -1j / 4]))
+    A.verify()
+
+@pytest.mark.xfail
+def test_global_phase():
+    A = ConstantVector(np.array([1, -1]))
+    A.verify()
+    A = ConstantVector(np.array([-1, 1]))
     A.verify()
