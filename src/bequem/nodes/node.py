a01804fa0971c069f074434f04c642b829dbe2a9
Ram4Dev <ram.mosco@uni-a.de>
2025-07-14 14:49:56 +0200

    more compact syntax for simulate_norm method

--- src/bequem/nodes/node.py
+++ src/bequem/nodes/node.py
@@ -186,18 +186,8 @@ class Node(ABC):
         """
         Method to compute the norm of the wavefunction in the subspace.
         """
-
-        # Simulate the circuit with the given input
-        circuit = self.circuit
-        wavefunction = circuit.simulate(input=input)
-
-        # get the basis vectores spanning the subspace in which the wavefunction is defined
-        subspace = self.subspace_out
-        basis_functions_of_subspace = subspace.enumerate_basis()
-
-        # compute the norm of the wavefunction in the subspace
-        norm = np.linalg.norm([wavefunction[i] for i in basis_functions_of_subspace])
-
+        wavefunction = self.circuit.simulate(input=input)
+        norm = np.linalg.norm([wavefunction[i] for i in self.subspace_out.enumerate_basis()])
         return norm
 
     def tree(self, verbose: bool = False, tree: Tree | None = None, holes: list[Node] = []) -> Tree:
