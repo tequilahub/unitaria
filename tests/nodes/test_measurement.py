dde6c5483bd9fcd7635ac94ea865c322fbef4cc5
Ram4Dev <ram.mosco@uni-a.de>
2025-07-16 16:46:41 +0200

    test both 0 and 1 axes

--- tests/nodes/test_measurement.py
+++ tests/nodes/test_measurement.py
@@ -8,12 +8,10 @@ z_pauli_matrix = ConstantUnitary(np.array([[1, 0], [0, -1]]))
 
 def test_sum_1_norm():
     sum_1 = 0.75 * identity + 0.25 * z_pauli_matrix
-    sum_1_expected_result = ConstantUnitary(np.array([[1, 0], [0, 0.5]]))
-    assert pytest.approx(sum_1.simulate_norm()) == sum_1_expected_result.simulate_norm()
-    assert pytest.approx(sum_1.simulate_norm()) == 1.0
+    assert pytest.approx(sum_1.simulate_norm(input=0)) == 1.0
+    assert pytest.approx(sum_1.simulate_norm(input=1)) == 0.5
 
 def test_sum_2_norm():
     sum_2 = 0.5 * identity + 0.5 * z_pauli_matrix
-    sum_2_expected_result = ConstantUnitary(np.array([[1, 0], [0, 0]]))
-    assert pytest.approx(sum_2.simulate_norm()) == sum_2_expected_result.simulate_norm() 
-    assert pytest.approx(sum_2.simulate_norm()) == 1.0 
+    assert pytest.approx(sum_2.simulate_norm(input=0)) == 1.0
+    assert pytest.approx(sum_2.simulate_norm(input=1)) == 0.0
