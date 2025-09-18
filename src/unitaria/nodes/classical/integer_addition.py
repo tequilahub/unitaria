1d41d24412c8898501bd3cc59afbdc5030529a6f
Matthias Deiml <matthias.deiml@uni-a.de>
2025-09-18 13:35:59 +0200

    Create  superclass

--- src/unitaria/nodes/classical/integer_addition.py
+++ src/unitaria/nodes/classical/integer_addition.py
@@ -1,12 +1,12 @@
 import numpy as np
 
 from ..node import Node
-from unitaria.subspace import Subspace
+from .classical import Classical
 from unitaria.circuit import Circuit
 from unitaria.circuits.arithmetic import addition_circuit
 
 
-class IntegerAddition(Node):
+class IntegerAddition(Classical):
     """
     Node implementing the (wrapping) addition of two integers.
 
@@ -22,7 +22,7 @@ class IntegerAddition(Node):
     target_bits: int
 
     def __init__(self, source_bits: int, target_bits: int):
-        super().__init__(2 ** (source_bits + target_bits), 2 ** (source_bits + target_bits))
+        super().__init__(source_bits + target_bits, source_bits + target_bits)
         # TODO: Restriction is because the ancilla free construction needs two source bits.
         #  I know how to fix this but haven't implemented it yet.
         if source_bits < 2 or target_bits < source_bits:
@@ -36,34 +36,17 @@ class IntegerAddition(Node):
     def parameters(self) -> dict:
         return {"source_bits": self.source_bits, "target_bits": self.target_bits}
 
-    def _subspace_in(self) -> Subspace:
-        return Subspace(self.source_bits + self.target_bits)
+    def compute_classical(self, input: np.ndarray) -> np.ndarray:
+        input1 = input % 2**self.source_bits
+        input2 = input // 2**self.source_bits
+        input2 = (input2 + input1) % 2**self.target_bits
+        return input1 + input2 * 2**self.source_bits
 
-    def _subspace_out(self) -> Subspace:
-        return Subspace(self.source_bits + self.target_bits)
-
-    def _normalization(self) -> float:
-        return 1
-
-    def compute(self, input: np.ndarray) -> np.ndarray:
-        old_shape = input.shape
-        N = 2**self.source_bits
-        M = 2**self.target_bits
-        input = input.reshape((-1, M, N))
-        result = np.zeros_like(input)
-        for val in range(N):
-            result[:, :, val] += np.roll(input[:, :, val], val, axis=-1)
-        return result.reshape(old_shape)
-
-    def compute_adjoint(self, input: np.ndarray) -> np.ndarray:
-        old_shape = input.shape
-        N = 2**self.source_bits
-        M = 2**self.target_bits
-        input = input.reshape((-1, M, N))
-        result = np.zeros_like(input)
-        for val in range(N):
-            result[:, :, val] += np.roll(input[:, :, val], -val, axis=-1)
-        return result.reshape(old_shape)
+    def compute_reverse_classical(self, input: np.ndarray) -> np.ndarray:
+        input1 = input % 2**self.source_bits
+        input2 = input // 2**self.source_bits
+        input2 = (input2 - input1) % 2**self.target_bits
+        return input1 + input2 * 2**self.source_bits
 
     def _circuit(self) -> Circuit:
         source = range(self.source_bits)
