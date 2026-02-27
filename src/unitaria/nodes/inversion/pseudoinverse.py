Fix normalization of QSVT

src/unitaria/nodes/inversion/pseudoinverse.py
@@ -1,28 +1,29 @@
+import numpy as np
+
 from unitaria.nodes import Node
 from unitaria.nodes import ProxyNode
 from unitaria.nodes.basic.adjoint import Adjoint
-from unitaria.nodes.basic.scale import Scale
 from unitaria.nodes.qsvt.qsvt import QSVT
 from unitaria.poly_approx import inverse_poly
 
 
 class Pseudoinverse(ProxyNode):
     """
-    For 0 < epsilon <= 2 * delta <= 0.5, assuming the smallest nonzero singular
-    value of A is at least delta, this node implements the Moore-Penrose
-    pseudoinverse of A.
+    This node implements the Moore-Penrose pseudoinverse of ``A`` with the
+    given tolerance, if `condition` is the ratio between ``A.normalization`` and
+    ``A``s smallest singular value.
 
     Implements Theorem 41 from https://arxiv.org/abs/1806.01838
 
     :param A:
         The node to be inverted
     :param condition:
-        An upper bound on the inverse of nonzero singular values of A.
-        This is the same as the usual definition of the condition of a matrix iff
-        the largest singular value is one, i.e. if the block-encoding has optimal
-        subnormalization.
-    :param epsilon:
-        The maximum absolute error
+        An upper bound on the inverse of nonzero singular values of A. This is
+        the same as the usual definition of the condition of a matrix iff the
+        largest singular value is its normalization, i.e. if the block-encoding
+        has optimal subnormalization.
+    :param tolerance:
+        The absolute error tolerance
     :param guaranteed:
         Determines if the accuracy should be guaranteed using analytical
         bounds (ignoring numerical errors). If this is set to false, this
@@ -32,14 +33,34 @@ class Pseudoinverse(ProxyNode):
 
     # TODO: The condition should actually be the condition of the matrix,
     #   the dependence on the subnormalization should be hidden from the user.
-    def __init__(self, A: Node, condition: float, epsilon: float, guaranteed: bool = False):
-        assert 0 < epsilon <= 2 / condition <= 0.5
-
+    def __init__(self, A: Node, condition: float, tolerance: float, guaranteed: bool = False):
         super().__init__(A.dimension_in, A.dimension_out)
-
+        assert condition >= 1
+        assert 0 < tolerance < 0.5
         self.A = A
         self.condition = condition
-        self.poly = inverse_poly(1 / condition, epsilon, guaranteed)
+        self.tolerance = tolerance
+        self.guaranteed = guaranteed
+
+        # This is to make certain assumptions fit.
+        # It is ok to make the condition larger and the error tolerance smaller
+        # than what the user supplied
+        condition = max(condition, 4)
+        tolerance = min(tolerance, 2 / condition)
+
+        assert 0 < tolerance <= 2 / condition <= 0.5
+
+        self.poly = inverse_poly(1 / condition, tolerance, False) * condition
+
+        # Rescale the polynomial, so that it fits the input normalization
+        X = np.polynomial.Chebyshev([0, 1])
+        self.poly = self.poly(X / A.normalization) / A.normalization
 
     def definition(self):
-        return Scale(QSVT(A=Adjoint(self.A), coefficients=self.poly.coef, format="chebyshev"), self.condition)
+        return QSVT(A=Adjoint(self.A), coefficients=self.poly.coef, format="chebyshev")
+
+    def children(self) -> list[Node]:
+        return [self.A]
+
+    def parameters(self) -> dict:
+        return {"condition": self.condition, "tolerance": self.tolerance, "guaranteed": self.guaranteed}

src/unitaria/nodes/qsvt/qsvt.py
@@ -147,14 +147,15 @@ class QSVTCoefficients:
 
     :param data:
         Coefficients of a polynomial in a basis determined by ``format`` or angles
-        in R convention
+        in R convention. If a polynomial is given, it should be non-normalized, i.e.
+        the actual polynomial that should be applied to the matrix.
     :param format:
-        One of ``"angles"``, ``"chebyshev``, or ``"monomial"``
+        One of ``"angles"``, ``"chebyshev``, or ``"monomial"``.
     :param input_normalization":
         See below
 
     :ivar polynomial:
-        The normalized polynomial
+        The non-normalized polynomial
     :ivar input_normalization:
         Factor by which the x-axis of the polynomial is normalized, i.e. the
         normalization of the block encoding that is transformed
@@ -207,18 +208,18 @@ class QSVTCoefficients:
                     f'format may only be one of "angles", "monomial", or "chebyshev" (passed format={format})'
                 )
 
-            self.polynomial = self.polynomial(np.polynomial.Chebyshev([0, 1 / input_normalization]))
-
             parity = self.polynomial.degree() % 2
             np.testing.assert_allclose(
                 self.polynomial.coef[1 - parity :: 2], 0, atol=1e-8, err_msg="Polynomial does not have definite parity"
             )
-            self.polynomial.coef[1 - parity :: 2] = 0.0
 
-            subpolys = compute_angles(self.polynomial)
+            poly_input_normalized = self.polynomial(np.polynomial.Chebyshev([0, input_normalization]))
+            poly_input_normalized.coef[1 - parity :: 2] = 0.0
+
+            subpolys = compute_angles(poly_input_normalized)
             assert len(subpolys) == 1, "Polynomial does not have definite parity"
 
-            self.polynomial, self.angles, self.output_normalization = subpolys[0]
+            _poly_normalized, self.angles, self.output_normalization = subpolys[0]
 
     def degree(self) -> int:
         return self.polynomial.degree()
@@ -256,6 +257,10 @@ class QSVT(Node):
             return Subspace(registers=self.A.subspace_out.registers, zero_qubits=1)
 
     def _compute_internal(self, input: np.ndarray, compute, compute_adjoint) -> np.ndarray:
+        # This code uses self.coefficients.polynomial, which is already
+        # non-normalized. This means no normalization constants should appear
+        # here.
+
         # TODO: For now, the polynomial should either be odd or even
         # Probably this should be tested somewhere else
         Tnp = input
@@ -265,7 +270,7 @@ class QSVT(Node):
             assert np.isclose(self.coefficients.polynomial.coef[0], 0)
             output = np.zeros_like(compute(input))
         if self.coefficients.degree() >= 0:
-            Tn = compute(Tnp) / self.A.normalization
+            Tn = compute(Tnp)
         for n in range(1, self.coefficients.degree() + 1):
             if (self.coefficients.degree() + n) % 2 == 0:
                 output += Tn * self.coefficients.polynomial.coef[n]
@@ -273,11 +278,9 @@ class QSVT(Node):
                 assert np.isclose(self.coefficients.polynomial.coef[n], 0)
             if n < self.coefficients.degree():
                 if n % 2 == 0:
-                    Tnp, Tn = Tn, 2 * (compute(Tn) / self.A.normalization) - Tnp
+                    Tnp, Tn = Tn, 2 * compute(Tn) - Tnp
                 else:
-                    Tnp, Tn = Tn, 2 * (compute_adjoint(Tn) / self.A.normalization) - Tnp
-
-        output *= self.coefficients.output_normalization
+                    Tnp, Tn = Tn, 2 * compute_adjoint(Tn) - Tnp
         return output
 
     def compute(self, input: np.ndarray) -> np.ndarray:

tests/nodes/test_qsvt.py
@@ -29,13 +29,13 @@ def test_qsvt_coefficients():
     # Scaled identity
     c = QSVTCoefficients(np.array([0, 2]), "chebyshev", 1)
     np.testing.assert_allclose(c.angles[0], -c.angles[1], atol=1e-5)
-    np.testing.assert_allclose(c.polynomial.coef, np.array([0, 1]))
+    np.testing.assert_allclose(c.polynomial.coef, np.array([0, 2]))
     assert c.output_normalization == 2
 
     c = QSVTCoefficients(np.array([0, 1]), "chebyshev", 2)
     np.testing.assert_allclose(c.angles[0], -c.angles[1], atol=1e-5)
     np.testing.assert_allclose(c.polynomial.coef, np.array([0, 1]))
-    assert c.output_normalization == 1 / 2
+    assert c.output_normalization == 2
 
     # Amplitude amplificiation
     c = QSVTCoefficients(np.array([-np.pi / 2, np.pi / 2, np.pi / 2, -np.pi / 2]), "angles", 1)