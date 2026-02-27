Fix normalization of QSVT

src/unitaria/poly_approx.py
@@ -2,6 +2,8 @@ import numpy as np
 import scipy.special
 from numpy.polynomial.chebyshev import Chebyshev
 
+from unitaria.util import poly_sup_norm
+
 
 def erf_poly(k: float, epsilon: float, guaranteed: bool = False) -> Chebyshev:
     """
@@ -155,10 +157,11 @@ def inverse_poly(delta: float, epsilon: float, guaranteed: bool = False) -> Cheb
 
     poly = delta * _unscaled_inverse_poly(1 / delta, (epsilon / 3) / delta)
 
-    maxima = poly.deriv().roots()
-    maxima = maxima[np.abs((np.imag(maxima)) < 1e-6) & (np.abs(maxima) <= 1)]
-    pmax = np.max(np.abs(poly(np.concatenate((maxima, [-1, 1])))))
+    pmax = poly_sup_norm(poly)
 
-    rect = rect_poly(delta / 2, delta / 2, min(epsilon / 3, 1 / pmax), guaranteed)
+    if pmax > 1.5 * (1 / delta):
+        rect = rect_poly(delta / 2, delta / 2, min(epsilon / 3, 1 / pmax), guaranteed)
 
-    return poly * (1 - rect)
+        return poly * (1 - rect)
+    else:
+        return poly

tests/nodes/test_pseudoinverse.py
@@ -5,6 +5,7 @@ from unitaria.nodes.basic.projection import Projection
 from unitaria.nodes.constants.constant_unitary import ConstantUnitary
 from unitaria.nodes.inversion.pseudoinverse import Pseudoinverse
 from unitaria.subspace import ZeroQubit, ID
+from unitaria.verifier import verify
 
 
 def rot_matrix(angle: float) -> np.array:
@@ -20,7 +21,18 @@ def test_pseudoinverse():
         @ A
         @ Projection(subspace_from=Subspace([ZeroQubit(), ID, ID]), subspace_to=Subspace(bits=3))
     )
-    B_inv = Pseudoinverse(B, condition=10.0, epsilon=0.1)
+    B_inv = Pseudoinverse(B, condition=10.0, tolerance=0.1)
     C = B_inv @ B  # should be roughly a projector on all but the first basis states
-    result = C.compute(np.eye(4))
-    assert np.allclose(result, np.diag(np.array([0.0, 1.0, 1.0, 1.0])), atol=0.1)
+    verify(C, np.diag(np.array([0.0, 1.0, 1.0, 1.0])), atol=0.1)
+
+
+def test_pseudoinverse_normalization():
+    A = 2 * ConstantUnitary(rot_matrix(np.pi / 3))
+    B = (
+        Projection(subspace_from=Subspace(bits=1), subspace_to=Subspace([ZeroQubit()]))
+        @ A
+        @ Projection(subspace_from=Subspace([ZeroQubit()]), subspace_to=Subspace(bits=1))
+    )
+    B_inv = Pseudoinverse(B, condition=2.0, tolerance=0.1)
+    C = B_inv @ B
+    verify(C, np.array([[1.0]]), atol=0.1)