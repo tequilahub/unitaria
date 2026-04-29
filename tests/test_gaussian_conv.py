3ad688f80aab75218df7e592d121e49b5324b038
ohuettenhofer <oliver.huettenhofer@gmail.com>
2026-04-29 15:34:28 +0200

    Update gaussian_conv tests and example

--- tests/test_gaussian_conv.py
+++ tests/test_gaussian_conv.py
@@ -8,13 +8,11 @@ def test_1d_gaussian_conv():
     gaussian = np.exp(-(x**2))
     prep = ut.ConstantVector(np.append([0], np.sqrt(gaussian)))
     conv = (
-        ut.Projection(subspace_from=ut.Subspace("####"), subspace_to=ut.Subspace("0###"))
-        @ (ut.Identity(ut.Subspace("####")) & ut.Adjoint(prep))
-        @ (ut.ConstantIntegerAddition(bits=4, constant=-4) & ut.Identity(ut.Subspace("0###")))
+        (ut.Identity(ut.Subspace.from_dim(16)) & ut.Adjoint(prep))
+        @ (ut.ConstantIntegerAddition(bits=4, constant=-4) & ut.Identity(ut.Subspace.from_dim(8)))
         @ ut.IntegerAddition(source_bits=3, target_bits=4)
-        @ (ut.Identity(ut.Subspace("####")) & prep)
-        @ ut.Projection(subspace_from=ut.Subspace("0###"), subspace_to=ut.Subspace("####"))
-    )
+        @ (ut.Identity(ut.Subspace.from_dim(16)) & prep)
+    )[:8, :8]
     ut.verify(conv)
 
     input = np.linspace(0.0, 1.0, 8)
@@ -29,30 +27,15 @@ def test_2d_gaussian_conv():
     gaussian = np.exp(-(x**2))
     prep = ut.ConstantVector(np.append([0], np.sqrt(gaussian)))
 
-    # TODO: It should be possible to write this more neatly using the @ operator in the future,
-    # but currently this increases the number of qubits and significantly slows down the test
-    one_dim_conv = ut.Mul(
-        ut.Mul(
-            ut.Projection(subspace_from=ut.Subspace("###"), subspace_to=ut.Subspace("0##")),
-            ut.Mul(
-                ut.Identity(ut.Subspace("###")) & ut.Adjoint(prep),
-                ut.ConstantIntegerAddition(bits=3, constant=-2) & ut.Identity(ut.Subspace("##")),
-                skip_projection=True,
-            ),
-            skip_projection=True,
-        ),
-        ut.Mul(
-            ut.IntegerAddition(source_bits=2, target_bits=3),
-            ut.Mul(
-                ut.Identity(ut.Subspace("###")) & prep,
-                ut.Projection(subspace_from=ut.Subspace("0##"), subspace_to=ut.Subspace("###")),
-                skip_projection=True,
-            ),
-            skip_projection=True,
-        ),
-        skip_projection=True,
-    )
+    one_dim_conv = (
+        (ut.Identity(ut.Subspace.from_dim(8)) & ut.Adjoint(prep))
+        @ (ut.ConstantIntegerAddition(bits=3, constant=-2) & ut.Identity(ut.Subspace.from_dim(4)))
+        @ ut.IntegerAddition(source_bits=2, target_bits=3)
+        @ (ut.Identity(ut.Subspace.from_dim(8)) & prep)
+    )[:4, :4]
+
     two_dim_conv = one_dim_conv & one_dim_conv
+    print(f"qubits: {two_dim_conv.circuit().n_qubits}")
     ut.verify(two_dim_conv)
 
     input = np.outer(np.linspace(0.0, 1.0, 4), np.linspace(0.0, 1.0, 4))
